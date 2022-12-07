b3c5bf171702b5f6ab4f3e5f7020f047eb90f818
RobertoSimonini <robertosimonini1@gmail.com>
2022-12-07 15:13:30 +0100

    printed on console number between 1 and 100

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-console.log('OK JS');
+
+/* 
+? Scrivi un programma che stampi in console i numeri da 1 a 100, ma che
+? per i multipli di 3 stampi “Fizz” al posto del numero
+? per i multipli di 5 stampi “Buzz” al posto del numero.
+? Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz” al posto del numero.
+? Prima di partire a scrivere codice poniamoci qualche domanda:
+? Come faccio a sapere se un numero è divisibile per un altro?
+? Abbiamo visto qualcosa di particolare che possiamo usare?
+*/
+
+//---------------------------------------------------------------//
+
+for (let i = 1; i <=100; i++) {
+    console.log (i);
+};
