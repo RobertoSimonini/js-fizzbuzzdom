f6a36a44d4151761a901a9c55a32e13245b03ebd
RobertoSimonini <robertosimonini1@gmail.com>
2022-12-07 15:50:41 +0100

    added mulitple function

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 
-/* 
+/*
 ? Scrivi un programma che stampi in console i numeri da 1 a 100, ma che
 ? per i multipli di 3 stampi “Fizz” al posto del numero
 ? per i multipli di 5 stampi “Buzz” al posto del numero.
@@ -11,6 +11,20 @@
 
 //---------------------------------------------------------------//
 
+
 for (let i = 1; i <=100; i++) {
-    console.log (i);
+    //  console.log (i);
+     if (i % 3 == 0) {
+         i = fizz;
+     };
+
+     if (i % 5 == 0) {
+         i = 'Buzz';
+     };
+
+     if (i % 15 == 0) {
+         i = 'FizzBuzz';
+     };
+
+     console.log(i);
 };
